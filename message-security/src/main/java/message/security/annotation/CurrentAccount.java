b1b691ee9c503eae4bab12831f2302951e7531c9
孙昊 <sunhao.java@gmail.com>
2014-12-07 20:00:04 +0800

    spring mvc

---
+++ message-security/src/main/java/message/security/annotation/CurrentAccount.java
@@ -0,0 +1,11 @@
+package message.security.annotation;
+
+/**
+ * 标识controller需要当前用户这个参数.
+ *
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 2014-12-7 19:49
+ */
+public @interface CurrentAccount {
+}
