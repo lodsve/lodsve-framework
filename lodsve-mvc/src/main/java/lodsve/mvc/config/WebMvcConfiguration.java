f50ac4bba4f810e86548b4216ef05fb7a3b2e5c3
孙昊 <sunhao.java@gmail.com>
2017-11-30 15:11:37 +0800

    解决spring-mvc访问静态资源

--- lodsve-mvc/src/main/java/lodsve/mvc/config/WebMvcConfiguration.java
+++ lodsve-mvc/src/main/java/lodsve/mvc/config/WebMvcConfiguration.java
@@ -2,8 +2,14 @@ package lodsve.mvc.config;
 
 import lodsve.core.autoconfigure.annotations.EnableConfigurationProperties;
 import lodsve.properties.ServerProperties;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
+
+import java.util.Map;
 
 /**
  * web mvc 配置,扫描包路径.
@@ -15,4 +21,18 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"lodsve.mvc"})
 @EnableConfigurationProperties(ServerProperties.class)
 public class WebMvcConfiguration {
+    @Bean
+    public DefaultServletHttpRequestHandler defaultServletHttpRequestHandler() {
+        return new DefaultServletHttpRequestHandler();
+    }
+
+    @Bean
+    public SimpleUrlHandlerMapping simpleUrlHandlerMapping() {
+        SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+        Map<String, String> urlMap = new ManagedMap<>();
+        urlMap.put("/**", "defaultServletHttpRequestHandler");
+        simpleUrlHandlerMapping.setUrlMap(urlMap);
+
+        return simpleUrlHandlerMapping;
+    }
 }
