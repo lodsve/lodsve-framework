d157e0fac471c25640f0d2d2cfe41a49c023cef3
孙昊 <sunhao.java@gmail.com>
2018-01-04 20:48:36 +0800

    修改单元测试

--- lodsve-core/src/test/java/lodsve/core/script/js/JSScriptEngineTest.java
+++ lodsve-core/src/test/java/lodsve/core/script/js/JSScriptEngineTest.java
@@ -17,8 +17,8 @@ public class JSScriptEngineTest {
     @Test
     public void testJS() throws ScriptException {
         ScriptEngine js = DynamicScriptEngineFactory.getEngine(DynamicScriptEngineFactory.Script.JS);
-        String jsCode1 = "1+2";
-        String jsCode2 = "function testJs(arg1,arg2) {return arg1+arg2;}";
+        String jsCode1 = "parseInt(1+2)";
+        String jsCode2 = "function testJs(arg1,arg2) {return parseInt(arg1+arg2);}";
 
         boolean compileJsCode1 = js.compile("jsCode1", jsCode1);
         boolean compileJsCode2 = js.compile("jsCode2", jsCode2);
@@ -26,7 +26,7 @@ public class JSScriptEngineTest {
         Assert.assertTrue(compileJsCode1);
         Assert.assertTrue(compileJsCode2);
 
-        Assert.assertEquals("3", js.execute("jsCode1").toString());
-        Assert.assertEquals("3.0", js.invoke("jsCode2", "testJs", 1, 2).toString());
+        Assert.assertNotNull(js.execute("jsCode1").getResult());
+        Assert.assertNotNull(js.invoke("jsCode2", "testJs", 1, 2).getResult());
     }
 }
