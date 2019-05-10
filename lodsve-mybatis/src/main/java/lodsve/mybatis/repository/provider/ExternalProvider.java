7360a4a33c32da6adb059fbb7e9d1af106f881a1
孙昊 <sunhao.java@gmail.com>
2019-05-11 01:04:51 +0800

    copyright

--- lodsve-mybatis/src/main/java/lodsve/mybatis/repository/provider/ExternalProvider.java
+++ lodsve-mybatis/src/main/java/lodsve/mybatis/repository/provider/ExternalProvider.java
@@ -1,10 +1,20 @@
 /*
- * Copyright (C) 2018, All rights Reserved, Designed By www.xiniaoyun.com
- * @author: 孙昊
- * @date: 2018-10-30 18:00
- * @Copyright: 2018 www.xiniaoyun.com Inc. All rights reserved.
- * 注意：本内容仅限于南京微欧科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ * Copyright (C) 2018  Sun.Hao
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package lodsve.mybatis.repository.provider;
 
 import java.util.List;
