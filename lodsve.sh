2fbb574d520cf32a51b0777a27fd377614b08179
孙昊 <sunhao.java@gmail.com>
2020-03-19 10:53:46 +0100

    add github repo,deploy release

--- lodsve.sh
+++ lodsve.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 # Help info function
-help() {
+help(){
   echo "--------------------------------------------------------------------------"
   echo ""
-  echo "usage: ./lodsve.sh [versions | deploy]"
+  echo "usage: ./lodsve.sh [versions | deployOss | deployGithub]"
   echo ""
-  echo "-versions    Update lodsve-framework versions."
-  echo "-deploy      Deploy lodsve-framework to maven repository"
+  echo "versions          Update lodsve-framework versions."
+  echo "deployOss         Deploy lodsve-framework to maven repository"
+  echo "deployGithub      Deploy lodsve-framework to github repository"
   echo ""
   echo "--------------------------------------------------------------------------"
 }
 
+
 # Start
 ./.mvn/logo.sh
 case "$1" in
-'versions')
-  ./.mvn/versions.sh $2
-  ;;
-'deploy')
-  ./.mvn/deploy.sh $2
-  ;;
-*)
-  help
-  ;;
+  'versions')
+    ./.mvn/versions.sh $2
+	;;
+  'deployOss')
+    ./.mvn/deploy.sh release-oss $2
+	;;
+  'deployGithub')
+    ./.mvn/deploy.sh release-github $2
+	;;
+  *)
+    help
 esac
